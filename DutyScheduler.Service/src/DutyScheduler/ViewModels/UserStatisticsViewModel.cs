d231d361eda32d452ed277f1a8a0c268c0db7a6c
Antonia Elek <antoniaelek@hotmail.com>
2017-01-12 20:17:31 +0100

    GET /statistics

---
+++ DutyScheduler.Service/src/DutyScheduler/ViewModels/UserStatisticsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using DutyScheduler.Models;
+
+namespace DutyScheduler.ViewModels
+{
+    public class UserStatisticsViewModel
+    {
+        public IEnumerable<Shift> Shifts { get; set; }
+        public IEnumerable<ReplacementHistory> Replaced { get; set; }
+        public IEnumerable<ReplacementHistory> Replacing { get; set; }
+    }
+}
