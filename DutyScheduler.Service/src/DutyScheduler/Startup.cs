4adcfad65fc37d8ddc1cc136c22f1f1a991f2c5d
Magdalena Petak <magdalena.petak@fer.hr>
2017-01-15 13:42:56 +0100

    take startup.cs from service branch

--- DutyScheduler.Service/src/DutyScheduler/Startup.cs
+++ DutyScheduler.Service/src/DutyScheduler/Startup.cs
@@ -13,141 +13,172 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Swashbuckle.Swagger.Model;
 using DutyScheduler.Middlewares;
-using Microsoft.AspNetCore.Routing;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using DutyScheduler.ViewModels;
+using Microsoft.AspNetCore.Identity;
 
 namespace DutyScheduler
 {
-	public class Startup
-	{
+    public class Startup
+    {
+        
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .AddUserSecrets();
 
-		public Startup(IHostingEnvironment env)
-		{
-			// Set up configuration sources.
-			var builder = new ConfigurationBuilder()
-				.SetBasePath(env.ContentRootPath)
-				.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-				.AddEnvironmentVariables()
-				.AddUserSecrets();
+            Configuration = builder.Build();
 
-			Configuration = builder.Build();
+            // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+            // builder.AddUserSecrets();
+        }
 
-			// For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
-			// builder.AddUserSecrets();
-		}
+        public IConfigurationRoot Configuration { get; set; }
 
-		public IConfigurationRoot Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var pathToDoc = Configuration["Swagger:Path"];
 
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-		public void ConfigureServices(IServiceCollection services)
-		{
-			var pathToDoc = Configuration["Swagger:Path"];
+            services.AddMvc();
 
-			services.AddCors(options =>
-			{
-				options.AddPolicy("CorsPolicy", builder => builder
-				.AllowAnyOrigin()
-				.AllowAnyMethod()
-				.AllowAnyHeader()
-				.AllowCredentials());
-			});
+            // Use a PostgreSQL database
+            var sqlConnectionString = Configuration.GetConnectionString("Postgres");
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseNpgsql(
+                    sqlConnectionString,
+                    b => b.MigrationsAssembly("DutyScheduler")
+                )
+            );
 
-			services.AddMvc();
-			services.Configure<MvcOptions>(options => options.Filters.Add(new CorsAuthorizationFilterFactory("CorsPolicy")));
-
-			// Use a PostgreSQL database
-			var sqlConnectionString = Configuration.GetConnectionString("Postgres");
-			services.AddDbContext<ApplicationDbContext>(options =>
-				options.UseNpgsql(
-					sqlConnectionString,
-					b => b.MigrationsAssembly("DutyScheduler")
-				)
-			);
-
-			services.AddSwaggerGen();
-			services.ConfigureSwaggerGen(options =>
-			{
-				options.SingleApiVersion(new Info
-				{
-					Version = "v1",
-					Title = "Duty Scheduler API",
-					Description = "Duty Scheduler API",
-					TermsOfService = "None"
-				});
-				options.IncludeXmlComments(pathToDoc);
-				options.DescribeAllEnumsAsStrings();
-			});
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Duty Scheduler API",
+                    Description = "Duty Scheduler API",
+                    TermsOfService = "None"
+                });
+                options.IncludeXmlComments(pathToDoc);
+                options.DescribeAllEnumsAsStrings();
+            });
 
 
-			services.AddIdentity<User, IdentityRole>(o =>
-			{
-				o.Password.RequireDigit = false;
-				o.Password.RequireLowercase = false;
-				o.Password.RequireUppercase = false;
-				o.Password.RequireNonAlphanumeric = false;
-				o.Password.RequiredLength = 4;
-				o.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents
-				{
-					OnRedirectToLogin = ctx =>
-					{
-						if (ctx.Request.Path.StartsWithSegments("/api") &&
-							ctx.Response.StatusCode == (int)HttpStatusCode.OK)
-						{
-							ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-						}
-						else
-						{
-							ctx.Response.Redirect(ctx.RedirectUri);
-						}
-						return Task.FromResult(0);
-					}
-				};
-			})
-				.AddEntityFrameworkStores<ApplicationDbContext>()
-				.AddDefaultTokenProviders();
+            services.AddIdentity<User, IdentityRole>(o =>
+            {
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 4;
+                o.Cookies.ApplicationCookie.Events = new CookieAuthenticationEvents
+                {
+                    OnRedirectToLogin = ctx =>
+                    {
+                        if (ctx.Request.Path.StartsWithSegments("/api") &&
+                            ctx.Response.StatusCode == (int)HttpStatusCode.OK)
+                        {
+                            ctx.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            //var body = "{\"" + Constants.Unauthorized.Key + "\":\"" + Constants.Unauthorized.Value + "\"}";
+                            //var json = new
+                            //{
+                            //    errors = "bla"
+                            //};
+                            //ctx.Response.Body = json.ToString().GenerateStreamFromString();
+                        }
+                        else
+                        {
+                            ctx.Response.Redirect(ctx.RedirectUri);
+                        }
+                        return Task.FromResult(0);
+                    }
+                };
+            })
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
-			services.AddMvcCore();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowNeeded",
+                    builder =>
+                    {
+                        builder
+                        .AllowCredentials();
+                    });
+            });
 
-			services.AddOptions();
+            services.AddMvcCore();
 
-			services.Configure<SmtpClientConfiguration>(Configuration.GetSection("SMTP"));
+            services.AddOptions();
 
-			//services.AddTransient<IEmailSender, AuthMessageSender>();
-		}
+            services.Configure<SmtpClientConfiguration>(Configuration.GetSection("SMTP"));
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
-		{
-			loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-			loggerFactory.AddDebug();
+            //services.AddTransient<IEmailSender, AuthMessageSender>();
+        }
 
-			//app.UseIISIntegration();
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
 
-			app.UseCors("CorsPolicy");
+            //app.UseIISIntegration();
 
-			DefaultFilesOptions options = new DefaultFilesOptions();
-			options.DefaultFileNames.Clear();
-			options.DefaultFileNames.Add("index.html");
-			app.UseDefaultFiles(options);
-			app.UseStaticFiles();
+            DefaultFilesOptions options = new DefaultFilesOptions();
+            options.DefaultFileNames.Clear();
+            options.DefaultFileNames.Add("index.html");
+            app.UseDefaultFiles(options);
+            app.UseStaticFiles();
 
-			app.UseIdentity().UseCookieAuthentication();
-
+            app.UseIdentity().UseCookieAuthentication();
+            //app.UseMiddleware<FillResponseBodyMiddleware>();
             app.UseWebSockets();
             app.UseWebSocketHandler();
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=home}/{action=index}/{id?}");
-			});
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=home}/{action=index}/{id?}");
+            });
 
-			app.UseSwagger();
-			app.UseSwaggerUi();
-		}
-	}
+            app.UseSwagger();
+            app.UseSwaggerUi();
+
+            try
+            {
+                using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+                    .CreateScope())
+                {
+                    serviceScope.ServiceProvider.GetService<ApplicationDbContext>()
+                         .Database.Migrate();
+
+                    var userManager = app.ApplicationServices.GetService<UserManager<User>>();
+                    serviceScope.ServiceProvider.GetService<ApplicationDbContext>().EnsureSeedData(userManager, DefaultAdmin(), Configuration["Admin:Password"]);
+                }
+            }
+            catch { }
+        }
+
+        private RegisterViewModel DefaultAdmin()
+        {
+            return new RegisterViewModel()
+            {
+                UserName = Configuration["Admin:Username"],
+                Name = Configuration["Admin:Username"],
+                LastName = Configuration["Admin:LastName"],
+                Password = Configuration["Admin:Password"],
+                Email = Configuration["Admin:Email"],
+                Phone= Configuration["Admin:Phone"],
+                Office = Configuration["Admin:Office"]
+        };
+        }
+    }
 }
